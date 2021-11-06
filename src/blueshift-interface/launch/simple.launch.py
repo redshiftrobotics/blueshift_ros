4a7a16fddfdeb67cc0888e67791fd9216f4b23d2
Kavi Dey <kaviasher@gmail.com>
2021-11-05 20:04:13 -0700

    Add ros2-web-bridge to launch file

--- src/blueshift-interface/launch/simple.launch.py
+++ src/blueshift-interface/launch/simple.launch.py
@@ -1,7 +1,8 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.launch_description_entity import LaunchDescriptionEntity
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -15,28 +16,21 @@ def generate_launch_description():
 
     web_interface_share_directory = get_package_share_directory('blueshift-interface')
 
+    # Web Bridge Launcher
     start_web_bridge_file = os.path.join(
         web_interface_share_directory,
-        'ros2-web-bridge',
         'bin',
-        'rosbridge.js'
+        'launch_ros2-web-bridge.sh',
     )
 
-    # ROS2-Web-Bridge Launcher
-    start_web_bridge = Node(
-        name='ros2_web_bridge',
-        executable='nvm',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[
-            'exec',
-            '12',
-            f'"node {start_web_bridge_file}"'
-        ],
-        cwd=web_interface_share_directory
+    start_web_bridge = ExecuteProcess(
+        cmd = [start_web_bridge_file, os.path.join(web_interface_share_directory, 'ros2-web-bridge', 'bin','rosbridge.js')],
+        name = 'ros2-web-bridge',
+        log_cmd = True,
+        shell = True
     )
     ld.add_action(start_web_bridge)
-
+    
     # Web Interface Launcher
     start_interface_file = os.path.join(
         web_interface_share_directory,
@@ -53,7 +47,7 @@ def generate_launch_description():
         ],
         cwd=web_interface_share_directory
     )
-    ld.add_action(start_web_interface)
+    #ld.add_action(start_web_interface)
 
 
     return ld
