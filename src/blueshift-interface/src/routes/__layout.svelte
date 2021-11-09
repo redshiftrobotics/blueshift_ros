602a5581dfc7a6dafe0113194a209dc7e2164006
Kavi Dey <kaviasher@gmail.com>
2021-11-09 00:20:32 -0800

    Update sketch to include feedback from haedyn

--- src/blueshift-interface/src/routes/__layout.svelte
+++ src/blueshift-interface/src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <slot />
 
-<style lang="scss" global>
-	//@import 'carbon-components-svelte/css/all.scss';
-    @import "carbon-components-svelte/css/all.css";
+<style lang="scss">
+	// @import 'carbon-components-svelte/css/all.scss';
+    @import 'carbon-components-svelte/css/all.css';
 </style>
