530c9a9f89bb0ee41e08dc432790f67e4bd69421
Kavi Dey <kaviasher@gmail.com>
2021-11-09 14:53:18 -0800

    Move the middle section out of the header and into index.svelte

--- src/blueshift-interface/src/components/header.svelte
+++ src/blueshift-interface/src/components/header.svelte
@@ -3,18 +3,58 @@
 		Header,
 		HeaderUtilities,
 		HeaderActionLink,
-		SkipToContent
+		SkipToContent,
+		HeaderNavItem,
+		HeaderNavMenu,
+		HeaderAction,
+		ButtonSet,
+		Button,
+		ContentSwitcher,
+		Switch,
+		TooltipIcon,
+		HeaderGlobalAction,
+		HeaderNav
 	} from 'carbon-components-svelte';
 	import Settings20 from 'carbon-icons-svelte/lib/Settings20';
 	import Debug20 from 'carbon-icons-svelte/lib/Debug20';
 	import SettingsAdjust20 from 'carbon-icons-svelte/lib/SettingsAdjust20';
 	import Drone20 from 'carbon-icons-svelte/lib/Drone20';
+	import Power20 from 'carbon-icons-svelte/lib/Power20';
+	import Restart20 from 'carbon-icons-svelte/lib/Restart20';
+	import Misuse20 from 'carbon-icons-svelte/lib/Misuse20';
+	import MisuseOutline20 from 'carbon-icons-svelte/lib/MisuseOutline20';
+	import WarningAlt20 from 'carbon-icons-svelte/lib/WarningAlt20';
+	import WarningAltFilled20 from 'carbon-icons-svelte/lib/WarningAltFilled20';
 </script>
 
 <Header company="Blueshift" platformName="Robotics">
 	<div slot="skip-to-content">
 		<SkipToContent />
 	</div>
+	<!-- Center buttons -->
+	<!-- I don't know if this is the correct way to add extra header elements, but it works lol -->
+	<HeaderUtilities>
+		<!-- Add spacing to the right side with `--cds-spacing-05` otherwise they touch the divider -->
+		<div style="padding-right: var(--cds-spacing-05)">
+			<HeaderGlobalAction icon={Power20} />
+			<HeaderGlobalAction icon={Restart20} />
+		</div>
+
+		{#if $$slots.middle_section}
+			<!-- Add spacing to the right side with `--cds-spacing-05` otherwise they touch the divider -->
+			<div style="padding-right: var(--cds-spacing-05)">
+				<slot name="middle_section" />
+			</div>
+		{/if}
+
+		<HeaderNav>
+			<HeaderAction icon={MisuseOutline20} closeIcon={MisuseOutline20} />
+			<!-- icon and closeIcon should switch to the filled in version and add error/warning counter when there are errors/warnings -->
+			<HeaderAction icon={WarningAlt20} closeIcon={WarningAlt20} />
+		</HeaderNav>
+	</HeaderUtilities>
+
+	<!-- Right side buttons -->
 	<HeaderUtilities>
 		<HeaderActionLink icon={Drone20} href="/" />
 		<HeaderActionLink icon={SettingsAdjust20} href="/copilot" />
--- src/blueshift-interface/src/routes/index.svelte
+++ src/blueshift-interface/src/routes/index.svelte
@@ -1,9 +1,25 @@
 <script>
-	import { Content, Grid, Row, Column } from 'carbon-components-svelte';
+	import { Content, Grid, Row, Column, HeaderNav, HeaderNavMenu, HeaderNavItem, HeaderGlobalAction } from 'carbon-components-svelte';
+	import Switcher20 from 'carbon-icons-svelte/lib/Switcher20';
 	import Header from '../components/header.svelte';
+
+	export let selectedCamera = "Loading...";
+	export let cameras = [];
 </script>
 
-<Header />
+<Header>
+	<HeaderNav slot="middle_section">
+		<HeaderNavMenu bind:text={selectedCamera}>
+			{#each cameras as camera}
+				<HeaderNavItem
+					text="{camera}"
+					href="#"
+				/>
+			{/each}
+		</HeaderNavMenu>
+		<HeaderGlobalAction icon={Switcher20} href="/" />
+	</HeaderNav>
+</Header>
 
 <Content>
 	<Grid>
