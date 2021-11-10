f55f65938f797f70209e7e7413661d13e8ed243f
Kavi Dey <kaviasher@gmail.com>
2021-11-09 18:02:09 -0800

    [not finished] start implementing custom error/warning header

---
+++ src/blueshift-interface/src/components/error_warning_status.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Header } from 'carbon-components-svelte';
+
+	import Misuse20 from 'carbon-icons-svelte/lib/Misuse20';
+	import MisuseOutline20 from 'carbon-icons-svelte/lib/MisuseOutline20';
+	import WarningAlt20 from 'carbon-icons-svelte/lib/WarningAlt20';
+	import WarningAltFilled20 from 'carbon-icons-svelte/lib/WarningAltFilled20';
+</script>
+
+<MisuseOutline20 style="fill: var(--cds-support-01)" />
+<span class="span-text" style="margin-right: var(--cds-spacing-04);">1</span>
+<WarningAlt20 style="fill: var(--cds-support-03)" />
+<span class="span-text">1</span>
+
+<style>
+	.span-text {
+		vertical-align: top;
+		margin-left: var(--cds-spacing-01);
+	}
+</style>
