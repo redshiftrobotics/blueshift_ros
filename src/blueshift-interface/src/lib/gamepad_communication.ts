8c458724c71ccf85fbcf008579e346e59836ddbb
Kavi Dey <kaviasher@gmail.com>
2021-11-24 23:52:29 -0800

    Zero out gamepad when it disconnects

--- src/blueshift-interface/src/lib/gamepad_communication.ts
+++ src/blueshift-interface/src/lib/gamepad_communication.ts
@@ -56,6 +56,7 @@ export function registerGamepadConnectedListener(callback: (event: GamepadEvent)
 	}
 	window.addEventListener('gamepadconnected', (event: GamepadEvent) => {
 		callback(event);
+		gamepadConnected = true;
 	});
 }
 
@@ -72,11 +73,14 @@ export function registerGamepadDisconnectedListener(callback: (event: GamepadEve
 		throw new Error('window is undefined. This must be called within onMount()');
 	}
 	window.addEventListener('gamepaddisconnected', (event: GamepadEvent) => {
+		gamepadConnected = false;
 		callback(event);
 	});
 }
 
+// TODO: update this to work with multiple gamepads (wkeep track of them by their id, etc.)
 let lastGamepadState: GamepadState = new GamepadState();
+let gamepadConnected = false;
 
 /**
  * returns 0 for any number within the threshold, and the number otherwise
@@ -99,50 +103,53 @@ function threshold(value: number, threshold: number): number {
  */
 function getGamepadState(gamepad: Gamepad, th: number): GamepadState {
 	const state = new GamepadState();
-	state.left.stick.x = threshold(gamepad.axes[0], th);
-	state.left.stick.y = threshold(gamepad.axes[1], th);
-	state.left.stick.button.pressed = gamepad.buttons[0].pressed;
-	state.left.stick.button.justPressed =
-		gamepad.buttons[0].pressed && !lastGamepadState.left.stick.button.pressed;
-	state.left.bumper.pressed = gamepad.buttons[5].pressed;
-	state.left.bumper.justPressed =
-		gamepad.buttons[5].pressed && !lastGamepadState.left.bumper.pressed;
-	state.left.trigger = threshold(gamepad.buttons[6].value, th);
-	state.right.stick.x = threshold(gamepad.axes[2], th);
-	state.right.stick.y = threshold(gamepad.axes[3], th);
-	state.right.stick.button.pressed = gamepad.buttons[1].pressed;
-	state.right.stick.button.justPressed =
-		gamepad.buttons[1].pressed && !lastGamepadState.right.stick.button.pressed;
-	state.right.bumper.pressed = gamepad.buttons[4].pressed;
-	state.right.bumper.justPressed =
-		gamepad.buttons[4].pressed && !lastGamepadState.right.bumper.pressed;
-	state.right.trigger = threshold(gamepad.buttons[7].value, th);
-	state.dpad.up = gamepad.buttons[12].pressed;
-	state.dpad.down = gamepad.buttons[13].pressed;
-	state.dpad.left = gamepad.buttons[14].pressed;
-	state.dpad.right = gamepad.buttons[15].pressed;
-	state.buttons.a.pressed = gamepad.buttons[2].pressed;
-	state.buttons.a.justPressed = gamepad.buttons[2].pressed && !lastGamepadState.buttons.a.pressed;
-	state.buttons.b.pressed = gamepad.buttons[3].pressed;
-	state.buttons.b.justPressed = gamepad.buttons[3].pressed && !lastGamepadState.buttons.b.pressed;
-	state.buttons.x.pressed = gamepad.buttons[0].pressed;
-	state.buttons.x.justPressed = gamepad.buttons[0].pressed && !lastGamepadState.buttons.x.pressed;
-	state.buttons.y.pressed = gamepad.buttons[1].pressed;
-	state.buttons.y.justPressed = gamepad.buttons[1].pressed && !lastGamepadState.buttons.y.pressed;
-	state.buttons.back.pressed = gamepad.buttons[8].pressed;
-	state.buttons.back.justPressed =
-		gamepad.buttons[8].pressed && !lastGamepadState.buttons.back.pressed;
-	state.buttons.start.pressed = gamepad.buttons[9].pressed;
-	state.buttons.start.justPressed =
-		gamepad.buttons[9].pressed && !lastGamepadState.buttons.start.pressed;
-	state.buttons.home.pressed = gamepad.buttons[10].pressed;
-	state.buttons.home.justPressed =
-		gamepad.buttons[10].pressed && !lastGamepadState.buttons.home.pressed;
+	if (gamepadConnected) {
+		state.left.stick.x = threshold(gamepad.axes[0], th);
+		state.left.stick.y = threshold(gamepad.axes[1], th);
+		state.left.stick.button.pressed = gamepad.buttons[0].pressed;
+		state.left.stick.button.justPressed =
+			gamepad.buttons[0].pressed && !lastGamepadState.left.stick.button.pressed;
+		state.left.bumper.pressed = gamepad.buttons[5].pressed;
+		state.left.bumper.justPressed =
+			gamepad.buttons[5].pressed && !lastGamepadState.left.bumper.pressed;
+		state.left.trigger = threshold(gamepad.buttons[6].value, th);
+		state.right.stick.x = threshold(gamepad.axes[2], th);
+		state.right.stick.y = threshold(gamepad.axes[3], th);
+		state.right.stick.button.pressed = gamepad.buttons[1].pressed;
+		state.right.stick.button.justPressed =
+			gamepad.buttons[1].pressed && !lastGamepadState.right.stick.button.pressed;
+		state.right.bumper.pressed = gamepad.buttons[4].pressed;
+		state.right.bumper.justPressed =
+			gamepad.buttons[4].pressed && !lastGamepadState.right.bumper.pressed;
+		state.right.trigger = threshold(gamepad.buttons[7].value, th);
+		state.dpad.up = gamepad.buttons[12].pressed;
+		state.dpad.down = gamepad.buttons[13].pressed;
+		state.dpad.left = gamepad.buttons[14].pressed;
+		state.dpad.right = gamepad.buttons[15].pressed;
+		state.buttons.a.pressed = gamepad.buttons[2].pressed;
+		state.buttons.a.justPressed = gamepad.buttons[2].pressed && !lastGamepadState.buttons.a.pressed;
+		state.buttons.b.pressed = gamepad.buttons[3].pressed;
+		state.buttons.b.justPressed = gamepad.buttons[3].pressed && !lastGamepadState.buttons.b.pressed;
+		state.buttons.x.pressed = gamepad.buttons[0].pressed;
+		state.buttons.x.justPressed = gamepad.buttons[0].pressed && !lastGamepadState.buttons.x.pressed;
+		state.buttons.y.pressed = gamepad.buttons[1].pressed;
+		state.buttons.y.justPressed = gamepad.buttons[1].pressed && !lastGamepadState.buttons.y.pressed;
+		state.buttons.back.pressed = gamepad.buttons[8].pressed;
+		state.buttons.back.justPressed =
+			gamepad.buttons[8].pressed && !lastGamepadState.buttons.back.pressed;
+		state.buttons.start.pressed = gamepad.buttons[9].pressed;
+		state.buttons.start.justPressed =
+			gamepad.buttons[9].pressed && !lastGamepadState.buttons.start.pressed;
+		state.buttons.home.pressed = gamepad.buttons[10].pressed;
+		state.buttons.home.justPressed =
+			gamepad.buttons[10].pressed && !lastGamepadState.buttons.home.pressed;
 
-	lastGamepadState = state;
+		lastGamepadState = state;
+	}
 
 	return state;
 }
+
 /**
  * Generates a readable store of the gamepad state
  * @param gamepad gamepad object get data from
