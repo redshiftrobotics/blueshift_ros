e0e7bf8d77b2d6a5a4773ff1fdcec27213f3665d
kavidey <kaviasher@gmail.com>
2022-05-16 00:09:20 -0700

    Clean up directory structure to work better with ROS
    Also start updating the svelte setup

--- blueshift_web/svelte.config.js
+++ blueshift_web/svelte.config.js
@@ -11,16 +11,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [preprocess(), optimizeImports()],
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		adapter: adapter({
 			// default options are shown
 			out: 'build',
 			precompress: true,
-			env: {
-				host: 'HOST',
-				port: 'PORT'
-			}
 		}),
 		vite: {
 			plugins: [
