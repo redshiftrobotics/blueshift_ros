a35d0beb95ba90efe2350e02a087f395e6c9eaa8
AchillesHam <achillesham@seattleacademy.org>
2022-06-21 15:28:28 -0700

    Add Control Node

---
+++ blueshift_core/src/blueshift_driver.cpp
@@ -0,0 +1,39 @@
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic2", 10);
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String &msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "yes '%s'", msg.data.c_str());
+      auto message = std_msgs::msg::String();
+      message.data = msg.data;
+      publisher_->publish(message);
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
