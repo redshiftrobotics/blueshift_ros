bc9b15c7745fbe49dfa8e704c637cc4f5bc2af6b
AchillesHam <achillesham@seattleacademy.org>
2022-06-23 17:08:33 -0700

    Not Working Code for Kavi to test :0

--- blueshift_core/src/blueshift_driver.cpp
+++ blueshift_core/src/blueshift_driver.cpp
@@ -7,23 +7,35 @@
 #include "holonomic.hpp"
 #include "blueshift_interfaces/msg/motors.hpp"
 
-class MinimalSubscriber : public rclcpp::Node
+class Control : public rclcpp::Node
 {
 public:
-  MinimalSubscriber()
+  Control()
       : Node("minimal_subscriber")
   {
+    this->declare_parameter("holonomic_speed_limiter", 0);
     publisher_ = this->create_publisher<blueshift_interfaces::msg::Motors>("motor_speeds", 10);
     subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "input", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
+        "input", 10, std::bind(&Control::topic_callback, this, std::placeholders::_1));
   }
+
+  void respond(){
+    this->get_parameter("holonomic_speed_limiter",parameter_value_);
+  }
+
+
 
 private:
   void topic_callback(const geometry_msgs::msg::Twist &msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I recieved Linear x:'%s'", std::to_string(msg.linear.x).c_str());
     auto message = blueshift_interfaces::msg::Motors();
-    Motors motor = holonomic_math(msg.linear.x, msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y, msg.angular.z);
+    
+    this->
+    Motors motor = holonomic_math(
+    msg.linear.x, msg.linear.y, msg.linear.z, 
+    msg.angular.x, msg.angular.y, msg.angular.z, 
+    parameter_value_);
 
     message.top_front_left = motor.top_front_left;
     message.top_front_right = motor.top_front_right;
@@ -36,14 +48,17 @@ private:
 
     publisher_->publish(message);
   }
+
+  int parameter_value_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
   rclcpp::Publisher<blueshift_interfaces::msg::Motors>::SharedPtr publisher_;
+
 };
 
 int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<Control>());
   rclcpp::shutdown();
   return 0;
 }
