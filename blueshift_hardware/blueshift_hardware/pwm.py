d52cc76a8b30350b9a3a66aae93a31584495c901
Aleah H <aleahjham@gmail.com>
2023-01-06 16:31:34 -0800

    Add pwm python node
    ros2 topic pub --once /motor_speeds blueshift_interfaces/msg/Motors "{'top_front_left': 1.0 , 'top_front_right': 0.0,'top_back_left': 0.0, 'top_back_right': 0.0, 'bottom_front_left': 0.0, 'bottom_front_right': 0.0, 'bottom_back_left': 0.0, 'bottom_back_right': 0.0 }"

---
+++ blueshift_hardware/blueshift_hardware/pwm.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+import board
+import busio
+import adafruit_pca9685
+
+from blueshift_interfaces.msg import Motors
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            Motors,
+            'motor_speeds',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.pca = adafruit_pca9685.PCA9685(self.i2c)
+        self.pca.frequency = 60
+
+        self.motor1 = self.pca.channels[0]
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.top_front_left)
+
+        # self.motor1.duty_cycle = msg.top_front_left
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
